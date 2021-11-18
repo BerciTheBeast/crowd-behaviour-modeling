58a2f668fe83400de54a607226f6d7e5a3f7185a
jaka-bernard <jaka.bernard@kalmia.si>
2021-11-18 21:40:52 +0100

    Initial navmesh + nav agent testing.

---
+++ Assets/Scripts/AgentControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentControl : MonoBehaviour
+{
+    public Transform destination;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.SetDestination(destination.position);
+    }
+
+}
