cf66025a1c3034d2ff5a17e01c0a4f27367b606a
jaka-bernard <jaka.bernard@kalmia.si>
2021-11-28 19:01:38 +0100

    Added grid calculation via box collider for every grid cell (should scale better with agent numbers than running the logic on agents). Really slow at 0.1 cell size.

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private Vector3 originPosition;
+    private int[,] gridArray;
+    private TextMesh[,] debugTextArray;
+
+    public Grid(int width, int height, float cellSize, Vector3 originPosition)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+    }
+
+    private Vector3 GetWorldPosition(int x, int y) {
+        return new Vector3(x, 0, y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).z / cellSize);
+    }
+
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = value;
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        } else
+        {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+
+    public void UpdateOccupancy() {
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                Vector3 pos = GetWorldPosition(x, y) + new Vector3(cellSize / 2, 0, cellSize / 2);
+                pos.y = 0.1f;
+                Collider[] hitColliders = Physics.OverlapBox(pos, new Vector3(cellSize / 2, 0, cellSize / 2), Quaternion.identity);
+                // Debug.Log("colliders for " + x + ", " + y + " (" + hitColliders.GetLength(0) + ")");
+                //for (int i = 0; i < hitColliders.GetLength(0); i++)
+                //{
+                //    Debug.Log(hitColliders[i]);
+                //}
+                if (hitColliders.GetLength(0) > 0) {
+                    SetValue(x, y, 1);
+                } else
+                {
+                    SetValue(x, y, 0);
+                }
+            }
+        }
+    }
+}
