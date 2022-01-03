e2524c7c34425e2149d5a6be6fb8ed76f5983b08
Andrej Hafner <andrej.hafn3r@gmail.com>
2022-01-03 13:41:51 +0100

    Added intersection scene

--- Assets/Scripts/AgentControl.cs
+++ Assets/Scripts/AgentControl.cs
@@ -10,7 +10,7 @@ public enum AgentBehaviourType
     GapSeeking,
     Following,
     StopAndGo,
-    Overtagking
+    Overtaking
 }
 
 public class AgentControl : MonoBehaviour
@@ -93,7 +93,7 @@ public class AgentControl : MonoBehaviour
 
     void StopAndGoBehaviour()
     {
-        if (behaviour == AgentBehaviourType.Default &&  Random.Range(1, 100) == 1) // TODO: Probability.
+        if (behaviour == AgentBehaviourType.Default &&  Random.Range(1, 1000) == 1) // TODO: Probability.
         {
             behaviour = AgentBehaviourType.StopAndGo;
             agent.isStopped = true;
